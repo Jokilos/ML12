211727fc1d5c49a644bb05bff0b6d2836682f73a
nojak <huhun@onet.pl>
2025-05-11 01:54:33 +0200

    added some logging

--- NLP/hw2/agents/two_agents.py
+++ NLP/hw2/agents/two_agents.py
@@ -4,16 +4,17 @@ from agents.prompts import expert_prompt, defuser_prompt
 from game_mcp.game_client import Defuser, Expert
 from agents.models import HFModel, SmollLLM
 
-def print_prompt(prompt, who):
-    print("-" * 30)
-    print(f" {who} ")
-    print("-" * 30)
-    print("\n" * 1)
+def print_prompt(prompt, who, verbose):
+    if verbose: 
+        print("-" * 30)
+        print(f" {who} ")
+        print("-" * 30)
+        print("\n" * 1)
 
-    if who == 'CONVO':
-        print(prompt)
-    else:
-        print(prompt[1]['content'])
+        if who == 'CONVO':
+            print(prompt)
+        else:
+            print(prompt[1]['content'])
 
 def cut_to_last_dot(text):
     return text[:text.rfind('.') + 1]
@@ -35,7 +36,12 @@ async def run_two_agents(
         defuser_model: HFModel,
         expert_model: HFModel,
         server_url: str = "http://0.0.0.0:8080",
-        max_new_tokens: int = 50
+        max_new_tokens: int = 50,
+        top_k = 50,
+        top_p = 0.9,
+        temperature = 0.7,
+        prompt_type = 'standard',
+        verbose = True,
 ) -> None:
     """
     Main coroutine that orchestrates two LLM agents (Defuser and Expert)
@@ -58,7 +64,6 @@ async def run_two_agents(
         while True:
             # 2) Defuser checks the bomb's current state
             bomb_state = await defuser_client.run("state")
-            print("[DEFUSER sees BOMB STATE]")
 
             if "Bomb disarmed!" in bomb_state or "Bomb exploded!" in bomb_state:
                 break
@@ -66,36 +71,34 @@ async def run_two_agents(
             # 3) Expert retrieves the relevant manual text
             manual_text = await expert_client.run()
             manual_text = '\n'.join(manual_text.strip().splitlines()[1:])
-            print("[EXPERT sees MANUAL]")
 
             # 4) Expert LLM uses the manual text + defuser’s question (bomb_state)
             #    to generate instructions
-            exp_messages = expert_prompt(manual_text, bomb_state)
-            print_prompt(exp_messages, 'EXPERT')
+            exp_messages = expert_prompt(manual_text, bomb_state, prompt_type)
+            print_prompt(exp_messages, 'EXPERT', verbose)
             expert_advice = expert_model.generate_response(
                 exp_messages,
                 max_new_tokens=max_new_tokens_expert,
-                temperature=0.7,
-                top_p=0.9,
-                top_k=50,
+                temperature=temperature,
+                top_p=top_p,
+                top_k=top_k,
                 do_sample=True,
                 keep_prompt=False,
             )
             expert_advice = expert_advice.replace("</think>", "").replace("\n", "")
 
             # 5) Defuser LLM uses the bomb state + expert advice to pick a single action
-            def_messages = defuser_prompt(bomb_state, expert_advice)
-            print_prompt(def_messages, 'DEFUSER')
+            def_messages = defuser_prompt(bomb_state, expert_advice, prompt_type)
+            print_prompt(def_messages, 'DEFUSER', verbose)
             def_action_raw = defuser_model.generate_response(
                 def_messages,
                 max_new_tokens=max_new_tokens,
-                temperature=0.7,
-                top_p=0.9,
-                top_k=50,
+                temperature=temperature,
+                top_p=top_p,
+                top_k=top_k,
                 do_sample=True,
                 keep_prompt=False,
             )
-            print("\n[DEFUSER ACTION RAW]:", def_action_raw)
 
             # 6) Attempt to extract a known command from def_action_raw
             #    If no recognized command is found, default to "help"
@@ -106,15 +109,18 @@ async def run_two_agents(
                 if is_action:
                     action = found_action 
                     break
-
-            print("\n[DEFUSER ACTION DECIDED]:", action)
+            
+            if verbose:
+                print("\n[DEFUSER ACTION DECIDED]:", action)
 
             # 7) Send that action to the server
             result = await defuser_client.run(action)
-            if action != 'help':
-                print("[SERVER RESPONSE]:")
-                print(result)
-            print("-" * 60)
+
+            if verbose:
+                if action != 'help': 
+                    print("[SERVER RESPONSE]:")
+                    print(result)
+                print("-" * 60)
 
             if "BOMB SUCCESSFULLY DISARMED" in result or "BOMB HAS EXPLODED" in result:
                 break
@@ -127,16 +133,17 @@ if __name__ == "__main__":
     import torch
     parser = argparse.ArgumentParser(description='Run MCP SSE-based ')
     parser.add_argument('--url', default = "http://localhost:8091/", help = 'URL to connect to')
+    parser.add_argument('--prompt', default = 'standard', help = 'propmt type')
+    parser.add_argument('--temp', type = float, default = 0.7,  help = 'temperature')
+    parser.add_argument('--topk', type = int, default = 50,  help = 'topk')
+    parser.add_argument('--topp', type = float, default = 0.9,  help = 'topp')
+    parser.add_argument('--verbose', type = int, default = 0,  help = 'verbosity')
     args = parser.parse_args()
 
     defuser_checkpoint: str = "Qwen/Qwen3-0.6B"
-    # defuser_checkpoint: str = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
-    # defuser_checkpoint: str = "Qwen/Qwen1.5-4B-Chat"
-    # defuser_checkpoint: str = "Qwen/Qwen2.5-1.5B-Instruct"
     expert_checkpoint: str = defuser_checkpoint
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    print(device)
     defuser_model = SmollLLM(defuser_checkpoint, device=device)
     expert_model = SmollLLM(expert_checkpoint, device=device)
 
@@ -146,5 +153,10 @@ if __name__ == "__main__":
             expert_model=expert_model,
             server_url=args.url,
             max_new_tokens=80,
+            top_k=args.topk,
+            top_p=args.topp,
+            temperature=args.temp,
+            prompt_type=args.prompt,
+            verbose=bool(args.verbose),
         )
     )
--- NLP/hw2/run_two.py
+++ NLP/hw2/run_two.py
@@ -2,12 +2,14 @@ import subprocess
 import time
 
 # This command's output is suppressed
+
 p1 = subprocess.Popen(["python", "-m", "game_mcp.game_server", "--port", "8091"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 time.sleep(0.1)
 
 # This command's output is shown
-p2 = subprocess.Popen(["python", "-m", "agents.two_agents"])
+args = ['--prompt', 'json', '--verbose', '1']
+p2 = subprocess.Popen(["python", "-m", "agents.two_agents"] + args)
 
 p1.wait()
 p2.wait()
