2e46d013e0a088e247e5458c8875535e80403d2f
Jokilos <huhun@onet.pl>
2025-06-07 14:58:47 +0200

    zso & rl1

--- ZSO/scripts/run_cow.sh
+++ ZSO/scripts/run_cow.sh
@@ -13,10 +13,10 @@ qemu-system-x86_64                                              \
     -chardev stdio,id=cons,signal=off                           \
     -device virtio-serial-pci                                   \
     -device virtconsole,chardev=cons                            \
-    # -kernel ../data/IMG/vmlinuz-6.12.6zsobpf                    \
-    # -initrd ../data/IMG/initrd.img-6.12.6zsobpf                 \
-    # -append "root=/dev/sda3"                                    \
-    # -display none                                               \
+    -kernel ../data/IMG/vmlinuz-6.12.6zsobpf                    \
+    -initrd ../data/IMG/initrd.img-6.12.6zsobpf                 \
+    -append "root=/dev/sda3"                                    \
+    -display none                                               \
 
 
     # stty rows 236 cols 64 
